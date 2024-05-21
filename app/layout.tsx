5bbff5ffdeff0de5c1da8052bf20ee841620a357
Samir <sobiroff23@gmail.com>
2024-05-21 23:57:42 +0500

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
+import { GoHomeFill } from "react-icons/go";
+import { RiMessengerLine } from "react-icons/ri";
+import { SlCompass } from "react-icons/sl";
+import { FaRegHeart } from "react-icons/fa";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,27 +25,27 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className}`}>
+            <body className= {`${inter.className}`  }  >
                 <header className="py-3 border-b-2 ">
-                    <div className="w-[95%] max-w-[1240px] flex justify-between items-center mx-auto my-0">
-                        <img className="w-[120px] h-[40px]" src="icons/Logo.png" alt="Logo" />
-                        <button className="px-20 py-2 flex items-center gap-1 bg-[#FAFAFA] border border-[#DBDBDB] text-[#9C9C9C]">
-                            {/* <CiSearch color="#9C9C9C" /> */}
+                    <div className="w-[95%] max-w-[1240px] flex justify-between items-center mx-auto my-0 ">
+                        <img className="w-[120px] h-[40px]" src="icons/Logo.png" color="white" alt="Logo"  />
+                        <button className="rounded-lg px-20 py-2 flex items-center gap-1 bg-[#FAFAFA] border border-[#DBDBDB] text-[#9C9C9C]">
+                            <CiSearch />
                             Search
                         </button>
 
                         <nav className="flex gap-3">
                             <button>
-                                {/* <GoHomeFill size={30} /> */}
+                                <GoHomeFill size={30} />
                             </button>
                             <button>
-                                {/* <LiaFacebookMessenger size={30} /> */}
+                                <RiMessengerLine size={30} />
                             </button>
                             <button>
-                                {/* <RiCompass3Line size={30} /> */}
+                                <SlCompass size={25} />
                             </button>
                             <button>
-                                {/* <IoMdHeartEmpty size={30} /> */}
+                                <FaRegHeart size={30} />
                             </button>
                             <button>
                                 <div className="w-[27px] h-[27px] rounded-full bg-[#c4c4c4]"></div>
