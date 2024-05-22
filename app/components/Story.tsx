e44bfdf91151fe61a363498824064802d19cda0f
Samir <sobiroff23@gmail.com>
2024-05-22 14:55:32 +0500

    fixed

--- app/components/Story.tsx
+++ app/components/Story.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function History(props: any) {
     return ( 
-        <div className="text-center">
+        <div className="text-center ">
             <div className="rounded-full p-1 border-2 border-[rgba(249,249,249,0.46)] ">
                 <div className="rounded-full h-12 w-12 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
     
